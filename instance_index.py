e01b8cfbc214d62ec646c42d34ee663f1850faa2
DELAB-Y <92504955+DELAB-Y@users.noreply.github.com>
2021-11-04 11:25:58 +0900

    Update instance_index.py

--- instance_index.py
+++ instance_index.py
@@ -3,7 +3,7 @@ import os
 # Get machine index for multi-instances data generation situations
 # We generated RocksDB data in GCP environment with multiple instances at the same time to accelerate the data generation speed
 def get_instance_index(count_file : int):
-    instance_index = os.popen("hostname | cut -d'-' -f3").read()[:-1] # "data-generation-1" get the index "1"
+    instance_index = os.popen("hostname | cut -d'-' -f3").read()[:-1] # 'data-generation-1' get the index '1'
     instance_count = int(instance_index) - 17 # -1 to the index of the instance
 
     # Set the parameter index range to fit the data generation process.
