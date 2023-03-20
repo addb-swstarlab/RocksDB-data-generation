1444aade1fdd8dfe4611e0b1d74abf0c2ee617c7
dkdl012 <31770302+dkdl012@users.noreply.github.com>
2023-03-20 19:49:37 +0900

    Update default_benchmark.sh

--- default_benchmark.sh
+++ default_benchmark.sh
@@ -1,9 +1,7 @@
 BENCHMARK=$1
 
 # generating directory
-DIR="/jieun/230319-rocksdb-complex-workload/results/default"
+DIR="results/default"
 [ ! -d $DIR ] && mkdir $DIR
-
-cd /jieun
 
 rocksdb/db_bench -key_size=32 -value_size=32768 -num=32736 -max_background_compactions=1 -max_background_flushes=1 -write_buffer_size=1048576 -max_write_buffer_number=2 -min_write_buffer_number_to_merge=1 -compaction_pri=0 -compaction_style=0 -level0_file_num_compaction_trigger=4 -level0_slowdown_writes_trigger=20 -level0_stop_writes_trigger=36 -compression_type=snappy -bloom_locality=0 -open_files=-1 -block_size=4096 -cache_index_and_filter_blocks=0 -max_bytes_for_level_base=4194304 -max_bytes_for_level_multiplier=10 -target_file_size_base=1048576 -target_file_size_multiplier=1 -num_levels=7 -memtable_bloom_size_ratio=0 -compression_ratio=0.5 --benchmarks="${BENCHMARK},compact,stats" --statistics > ${DIR}/${BENCHMARK}.txt
