505e3c9bdf2d2c9c38b8a3918cf5799387dd713f
dkdl012 <31770302+dkdl012@users.noreply.github.com>
2023-03-20 18:46:26 +0900

    Delete testing.sh

--- testing.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/bash
-
-cd /home/jieun/data_generation_rocksdb
-python main.py --wk $1
-cd /home/jieun
-tail -n 1 /home/jieun/data_generation_rocksdb/external_results_11.csv >> comp_results.csv
-
