6d67cf88273b3bf77efb6169c91263f51a318dc6
dkdl012 <dkdl031@naver.com>
2024-10-31 18:07:06 +0900

    241031

---
+++ params.py
@@ -0,0 +1,6 @@
+import os
+HOME_PATH = os.path.expanduser('~')
+PROJECT_NAME = 'RocksDB-data-generation'
+
+ROCKSDB_PATH = os.path.join(HOME_PATH, 'rocksdb')
+RESULT_PATH = os.path.join(HOME_PATH, PROJECT_PATH, 'results')
